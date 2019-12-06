29307f639f5156a2ea20dc61c55ec5a0671f612d
lamwilton <lamwiltonusa@gmail.com>
2019-12-06 14:54:22 -0800

    Done two

--- EC-master/ecListFuncs.cpp
+++ EC-master/ecListFuncs.cpp
@@ -1,5 +1,5 @@
-/*  Name:
- *  USC NetID:
+/*  Name: Ting Fung Lam
+ *  USC NetID: tingfunl
  *  CS 455 Fall 2019
  *
  *  See ecListFuncs.h for specification of each function.
@@ -16,19 +16,77 @@
 
 using namespace std;
 
-
+#include <iostream>
 void longestRun(ListType list, int & maxRunVal, int & maxRunLen) {
-   
+   maxRunVal = list->data;
+   maxRunLen = 1;
+   int runLen = 1;
+   int runVal = list->data;
+   ListType prev = list;
+   while (list->next != NULL) {
+      list = list->next;
+      if (prev->data == list->data) {
+         runLen++;
+         runVal = list->data;
+      }
+      if (runLen > maxRunLen) {
+         maxRunLen = runLen;
+         maxRunVal = runVal;
+      }
+      if (list->next != NULL && prev->data != list->data) {
+         runLen = 1;
+      }
+      prev = prev->next;
+   }
 }
 
 
 void removeMultiplesOf3(ListType & list) {
+   ListType curr = list;
+   ListType prev = list;
+   while (curr != NULL) {
+      //if first one deletes
+      if (prev == curr && curr->data % 3 == 0) {
+         //ListType temp = curr;
+         curr = curr->next;
+         prev = prev->next;
+         list = list->next;
+         //delete temp;
+      }
+      // if not first one deletes
+      else if (prev != curr && curr->data % 3 == 0) {
+         //ListType temp = curr;
+         prev->next = curr->next;
+         //delete temp;
+         curr = prev->next;
+      }
+      // if no need to delete
+      else {
+         if (prev != curr) {
+            prev = prev->next;
+         }
+         curr = curr->next;
+      }
+   }
+         
 
 }
 
 
 void insertMiddle(ListType & list, int midVal) {
-   
+   int length = 0;
+   ListType curr = list;
+   // Get length
+   while (curr != NULL) {
+      length++;
+      curr = curr->next;
+   }
+   int mid = length / 2;
+   curr = list;
+   for (int i = 0; i < mid - 1; i++) {
+      curr = curr->next;
+   }
+   // Insert
 }
 
 
