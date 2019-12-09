516bdb10ad72fd5f093f2092b445508da543e437
lamwilton <lamwiltonusa@gmail.com>
2019-12-09 14:27:31 -0800

    Finished

--- EC-master/ecListFuncs.cpp
+++ EC-master/ecListFuncs.cpp
@@ -16,7 +16,6 @@
 
 using namespace std;
 
-#include <iostream>
 void longestRun(ListType list, int & maxRunVal, int & maxRunLen) {
    maxRunVal = list->data;
    maxRunLen = 1;
@@ -75,6 +74,13 @@ void removeMultiplesOf3(ListType & list) {
 
 void insertMiddle(ListType & list, int midVal) {
    int length = 0;
+   
+   if (list == NULL) {
+      Node* newNode = new Node(midVal);
+      list = newNode;
+      return;
+   }
+   
    ListType curr = list;
    // Get length
    while (curr != NULL) {
@@ -87,11 +93,61 @@ void insertMiddle(ListType & list, int midVal) {
       curr = curr->next;
    }
    // Insert
+   Node* newNode = new Node(midVal, curr->next);
+   curr->next = newNode;
 }
 
 
 ListType merge(ListType list1, ListType list2) {
-   return NULL;  // dummy code so starter code compiles
+   // Merging list2 into list1
+   // If both are empty lists
+   if (list1 == NULL && list2 == NULL) {
+      return NULL;
+   }
+   // If list1 is empty
+   if (list1 == NULL) {
+      return list2;
+   }
+   ListType head = list1;
+   ListType prev = list1;
+
+   while (list1 != NULL || list2 != NULL) {
+      // if list1 is ending
+      if (list1 == NULL) {
+         prev->next = list2;
+         return head;
+      }
+      // if list2 is ending
+      if (list2 == NULL) {
+         return head;
+      }
+
+      if (list1 != NULL && list2 != NULL) {
+         if (list2->data < list1->data) {
+            // if the smallest number is in list2
+            if (prev == list1) {
+               prev = list2;
+               list2 = list2->next;
+               prev->next = list1;
+               head = prev;
+            }
+            else {               
+               prev->next = list2;
+               prev = prev->next;
+               list2 = list2->next;
+               prev->next = list1;
+            }
+         }
+         else {
+            if (prev != list1) {
+               prev = prev->next;
+            }
+            list1 = list1->next;
+         }
+      }
+   }
+      
+   return head;
 }
 
 
